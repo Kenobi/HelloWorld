50274e5c873fc9c9543550eb693b90a893bd87d1
Sergey Kuchmistov <dolphinarium90@yandex.ru>
2010-11-25 15:16:04 +0200

    hello world commit - eclipse

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 * @author Sergey Kuchmistov
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello");
+	}
+
+}
